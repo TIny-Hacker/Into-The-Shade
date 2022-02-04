9f37647b96c784691aa6371e71c651efcdc2fe7a
TIny_Hacker <newbbnews@gmail.com>
2022-02-04 17:33:21 -0500

    Added the shade

--- src/main.c
+++ src/main.c
@@ -5,23 +5,31 @@
 #include <keypadc.h>
 #include <stdbool.h>
 
-void car(x, y, direction) {
+#define TIMER_FREQ 32768
+#define ONE_SECOND (TIMER_FREQ / 1)
+#define HALF_SECOND (TIMER_FREQ / 2)
+#define THIRD_SECOND (TIMER_FREQ / 3)
+#define QRTR_SECOND (TIMER_FREQ / 4)
+
+void draw(shadeX, shadeY, carX, carY, direction) {
     gfx_SetDrawBuffer();
 
     gfx_FillScreen(2);
 
+    gfx_FillRectangle_NoClip(shadeX, shadeY, 80, 60);
+    
     switch (direction) {
         case 0:
-            gfx_ScaledTransparentSprite_NoClip(greenCarRight, x, y, 3, 3);
+            gfx_ScaledTransparentSprite_NoClip(greenCarRight, carX, carY, 3, 3);
             break;
         case 1:
-            gfx_ScaledTransparentSprite_NoClip(greenCarLeft, x, y, 3, 3);
+            gfx_ScaledTransparentSprite_NoClip(greenCarLeft, carX, carY, 3, 3);
             break;
         case 2:
-            gfx_ScaledSprite_NoClip(greenCarDown, x, y, 3, 3);
+            gfx_ScaledSprite_NoClip(greenCarDown, carX, carY, 3, 3);
             break;
         case 3:
-            gfx_ScaledSprite_NoClip(greenCarUp, x, y, 3, 3);
+            gfx_ScaledSprite_NoClip(greenCarUp, carX, carY, 3, 3);
             break;
         default:
             break;
@@ -35,38 +43,51 @@ int main(void) {
     gfx_Begin();
     gfx_SetPalette(global_palette, sizeof_global_palette, 0);
     gfx_SetTransparentColor(1);
+    gfx_SetColor(74);
 
-    uint8_t y = 4;
-    int x = 4;
+    uint8_t carY = 4;
+    int carX = 4;
+    uint8_t shadeY = 50;
+    int shadeX = 100;
+    uint8_t shadePath = 0;
     uint8_t direction = 0;
 
-    car(4, 4, 0);
+    rtc_Enable(RTC_SEC_INT);
+    srand(rtc_Time());
+
+    timer_Disable(1);
+    timer_Set(1, THIRD_SECOND);
+    timer_SetReload(1, THIRD_SECOND);
+
+    draw(100, 50, 4, 4, 0);
+
+    timer_Enable(1, TIMER_32K, TIMER_0INT, TIMER_DOWN);
 
     while (!kb_IsDown(kb_KeyClear)) {
         kb_Scan();
 
         switch (kb_Data[7]) {
             case kb_Up:
-                if (y > 4) {
-                    y -= 4;
+                if (carY > 4) {
+                    carY -= 4;
                 }
                 direction = 3;
                 break;
             case kb_Down:
-                if (y < 184) {
-                    y += 4;
+                if (carY < 184) {
+                    carY += 4;
                 }
                 direction = 2;
                 break;
             case kb_Left:
-                if (x > 4) {
-                    x -= 4;
+                if (carX > 4) {
+                    carX -= 4;
                 }
                 direction = 1;
                 break;
             case kb_Right:
-                if (x < 270) {
-                    x += 4;
+                if (carX < 270) {
+                    carX += 4;
                 }
                 direction = 0;
                 break;
@@ -74,8 +95,38 @@ int main(void) {
                 break;
         }
 
-        car(x, y, direction);
+        if (timer_ChkInterrupt(1, TIMER_RELOADED)) {
+            shadePath = randInt(0, 4);
+            timer_AckInterrupt(1, TIMER_RELOADED);
+        }
+
+        switch (shadePath) {
+            case 0:
+                if (shadeY > 4) {
+                    shadeY -= 2;
+                }
+                break;
+            case 1:
+                if (shadeY < 184) {
+                    shadeY += 4;
+                }
+                break;
+            case 2:
+                if (shadeX > 4) {
+                    shadeX -= 4;
+                }
+                break;
+            case 3:
+                if (shadeX < 270) {
+                    shadeX += 4;
+                }
+            default:
+                break;
+        }
+
+        draw(shadeX, shadeY, carX, carY, direction);
     }
+    timer_Disable(1);
 
     gfx_End();
 }
