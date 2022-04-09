4baea58fdfb2d38f639ddd98aabc89540490a4eb
TIny_Hacker <thetiniesthacker@gmail.com>
2022-04-09 17:51:53 -0400

    Added pond, removed the puddle

--- src/ui.c
+++ src/ui.c
@@ -98,9 +98,6 @@ void ui_BackgroundFrame(uint8_t day) {
     gfx_FillRectangle_NoClip(21, 19, 208, 198);
     gfx_SetColor(3);
     gfx_FillRectangle_NoClip(23, 21, 204, 194);
-    gfx_ScaledTransparentSprite_NoClip(roadPuddle, 154, 42, 2, 2);
-    gfx_ScaledTransparentSprite_NoClip(roadCrack, 83, 139, 2, 2);
-    gfx_ScaledTransparentSprite_NoClip(roadCrack, 56, 84, 2, 2);
     gfx_ScaledTransparentSprite_NoClip(flame, 281, 219, 2, 2);
     gfx_ScaledTransparentSprite_NoClip(stopwatch, 253, 219, 2, 2);
     gfx_SetTextScale(2, 2);
